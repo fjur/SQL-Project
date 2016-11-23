b2320bf4ec09131d29d640620bbc67d4d4994ceb
fjur <fjuristovski@gmail.com>
2016-11-22 22:02:32 -0800

    Reworked minor commenting and style

--- A.sql
+++ A.sql
@@ -1,6 +1,6 @@
 -- Switch to master to check and delete database
 USE master;
-
+-- Check if database exists, if it exists, delete it
 if exists (SELECT * FROM sysdatabases WHERE name='Cus_Orders')
 BEGIN
 	raiserror('Dropping existing Cus_Orders ....', 0, 1);
@@ -8,7 +8,8 @@ BEGIN
 END
 GO
 
--- CREATE Customer Orders DatabasPRINT '**********';
+-- CREATE Customer Orders Database
+PRINT '**********';
 PRINT '* A1 - Creating Cus_Orders Database *';
 
 CREATE DATABASE Cus_Orders;
@@ -26,25 +27,30 @@ BEGIN
 END
 GO
 
+-- DROP TYPE cusidt;
+-- DROP TYPE prodidt;
+-- DROP TYPE orderidt;
+-- DROP TYPE titleidt;
+
 PRINT '**********';
 PRINT '* A2 - Creating User Defined Data Types *';
 
-PRINT '* cusidt User Data Type created *';
+PRINT '* cusidt User Data Type created for customers table *';
 
 CREATE TYPE cusidt FROM char(5) NOT NULL;
 GO
 
-PRINT '* orderidt User Data Type created *';
+PRINT '* orderidt User Data Type created for orders table *';
 
 CREATE TYPE orderidt FROM int NOT NULL;
 GO
 
-PRINT '* prodidt User Data Type created *';
+PRINT '* prodidt User Data Type created for products table *';
 
 CREATE TYPE prodidt FROM int NOT NULL;
 GO
 
-PRINT '* titleidt User Data Type created *';
+PRINT '* titleidt User Data Type created for titles table *';
 
 CREATE TYPE titleidt FROM char(3) NOT NULL;
 GO
@@ -159,31 +165,57 @@ PRINT '* A4 - Creating tables Primary Keys and Foreign Keys *';
 
 PRINT '* Add PK to customers *';
 
+--Create the primary key for the customers table on customer_id
 ALTER TABLE customers
 ADD PRIMARY KEY (customer_id);
 GO
 
-
 PRINT '* Add PK to shippers *';
 
+--Create the primary key for the shippers table on shippers_id
 ALTER TABLE shippers
 ADD PRIMARY KEY (shipper_id);
 GO
 
 PRINT '* Add PK to suppliers *';
 
+--Create the primary key for the suppliers table on supplier_id
 ALTER TABLE suppliers
 ADD PRIMARY KEY (supplier_id);
 GO
 
 PRINT '* Add PK to titles *';
 
+--Create the primary key for the titles table on title_id
 ALTER TABLE titles
 ADD PRIMARY KEY (title_id);
 GO
 
+PRINT '* Add PK to orders *';
+
+--Create the primary key on orders table on order_id
+ALTER TABLE orders
+ADD PRIMARY KEY (order_id);
+GO
+
+PRINT '* Add PK to products *';
+
+--Create the primary key on products table on product_id
+ALTER TABLE products
+ADD PRIMARY KEY (product_id);
+GO
+
+PRINT '* Add PK to order_details *';
+
+--Create the composite primary key on order_details for (order_id, product_id)
+ALTER TABLE order_details
+ADD PRIMARY KEY (order_id, product_id);
+GO
+
 PRINT '* Add FK to customers *';
 
+--Create the foreign key for the customers table on title_id
+--References title_id in titles table
 ALTER TABLE customers
 ADD CONSTRAINT fk_customers_titles
 FOREIGN KEY (title_id)
@@ -191,26 +223,10 @@ REFERENCES titles
 (title_id);
 GO
 
-PRINT '* Add PK to orders *';
-
-ALTER TABLE orders
-ADD PRIMARY KEY (order_id);
-GO
-
-PRINT '* Add PK to products *';
-
-ALTER TABLE products
-ADD PRIMARY KEY (product_id);
-GO
-
-
-PRINT '* Add PK to order_details *';
-
-ALTER TABLE order_details
-ADD PRIMARY KEY (order_id, product_id);
-
 PRINT '* Add FK to orders *';
 
+--Create the foreign key for the orders table on customer_id
+--References customer_id in customers table
 ALTER TABLE orders
 ADD CONSTRAINT fk_orders_customer
 FOREIGN KEY (customer_id)
@@ -220,6 +236,8 @@ GO
 
 PRINT '* Add FK to orders *';
 
+--Create the foreign key for the orders table on shippers_id
+--References shipper_id in shippers table
 ALTER TABLE orders
 ADD CONSTRAINT fk_orders_shipper
 FOREIGN KEY (shipper_id)
@@ -229,6 +247,8 @@ GO
 
 PRINT '* Add FK to order_details *';
 
+--Create the foreign key for the order_details table on order_id
+--References order_id in orders table
 ALTER TABLE order_details
 ADD CONSTRAINT fk_order_details_order
 FOREIGN KEY (order_id)
@@ -238,6 +258,8 @@ GO
 
 PRINT '* Add FK to order_details *';
 
+--Create the foreign key for the order_details table on product_id
+--References product in products table
 ALTER TABLE order_details
 ADD CONSTRAINT fk_order_details_product
 FOREIGN KEY (product_id)
@@ -245,9 +267,10 @@ references products
 (product_id);
 GO
 
-
 PRINT '* Add FK to products *';
 
+--Create the foreign key for the products table on supplier_id
+--References supplier_id in suppliers table
 ALTER TABLE products
 ADD CONSTRAINT fk_products_supplier
 FOREIGN KEY (supplier_id)
@@ -260,6 +283,7 @@ PRINT '* A5 - Adding constraints to tables *';
 
 PRINT '* default country for customer table *';
 
+--Set default country for customers to 'Canada'
 ALTER TABLE customers
 ADD CONSTRAINT default_customers_country
 DEFAULT('Canada') FOR country;
@@ -267,27 +291,31 @@ Go
 
 PRINT '* default date for orders table *';
 
+--Set default date to 10 days later for orders required_date
 ALTER TABLE orders
 ADD CONSTRAINT default_order_date
 DEFAULT DATEADD(DAY, 10, GETDATE()) FOR required_date;
 GO
 
-PRINT '* check constraint for quantity on order_details table *';
+PRINT '* create constraint for quantity on order_details table *';
 
+--Create constraint, order_details quantity >= 1
 ALTER TABLE order_details
 ADD CONSTRAINT check__order_details_quantity
 CHECK(quantity >= 1);
 GO
 
-PRINT '* check constraint for reorder level on products table *';
+PRINT '* create constraint for reorder level on products table *';
 
+--Create constraint products reorder_level >= 1
 ALTER TABLE products
 ADD CONSTRAINT check_products_reorder
 CHECK (reorder_level >= 1);
 GO
 
-PRINT '* check constraint for quantity on products table *';
+PRINT '* create constraint for quantity on products table *';
 
+--Create constraint products quantity_in_stock < 150
 ALTER TABLE products
 ADD CONSTRAINT check_products_quantity
 CHECK (quantity_in_stock < 150);
@@ -295,7 +323,99 @@ GO
 
 PRINT '* default province for suppliers table *';
 
+--Set default province for suppliers to 'BC'
 ALTER TABLE suppliers
 ADD CONSTRAINT default_suppliers_province
 DEFAULT ('BC') for province;
 GO
+
+USE Cus_Orders;
+
+
+PRINT '**********';
+PRINT '* Inserting Values into database *';
+
+PRINT char(13) + '* Inserting Values into titles table *';
+--insert values into titles
+BULK INSERT titles 
+FROM 'C:\TextFiles\titles.txt' 
+WITH (
+               CODEPAGE=1252,                  
+		DATAFILETYPE = 'char',
+		FIELDTERMINATOR = '\t',
+		KEEPNULLS,
+		ROWTERMINATOR = '\n'	            
+	 )
+
+PRINT char(13) + '* Inserting Values into suppliers table *';
+--insert values into suppliers
+BULK INSERT suppliers 
+FROM 'C:\TextFiles\suppliers.txt' 
+WITH (  
+               CODEPAGE=1252,               
+		DATAFILETYPE = 'char',
+		FIELDTERMINATOR = '\t',
+		KEEPNULLS,
+		ROWTERMINATOR = '\n'	            
+	  )
+
+ PRINT char(13) + '* Inserting Values into shippers table *';
+--insert values into shippers
+BULK INSERT shippers 
+FROM 'C:\TextFiles\shippers.txt' 
+WITH (
+               CODEPAGE=1252,            
+		DATAFILETYPE = 'char',
+		FIELDTERMINATOR = '\t',
+		KEEPNULLS,
+		ROWTERMINATOR = '\n'	            
+	  )
+
+PRINT char(13) + '* Inserting Values into customers table *';
+--insert values into customers
+BULK INSERT customers 
+FROM 'C:\TextFiles\customers.txt' 
+WITH (
+               CODEPAGE=1252,            
+		DATAFILETYPE = 'char',
+		FIELDTERMINATOR = '\t',
+		KEEPNULLS,
+		ROWTERMINATOR = '\n'	            
+	  )
+
+PRINT char(13) + '* Inserting Values into products table *';
+--insert values into products
+BULK INSERT products 
+FROM 'C:\TextFiles\products.txt' 
+WITH (
+               CODEPAGE=1252,             
+		DATAFILETYPE = 'char',
+		FIELDTERMINATOR = '\t',
+		KEEPNULLS,
+		ROWTERMINATOR = '\n'	            
+	  )
+
+PRINT char(13) + '* Inserting Values into order_details table *';
+--insert values into order_details
+BULK INSERT order_details 
+FROM 'C:\TextFiles\order_details.txt'  
+WITH (
+               CODEPAGE=1252,              
+		DATAFILETYPE = 'char',
+		FIELDTERMINATOR = '\t',
+		KEEPNULLS,
+		ROWTERMINATOR = '\n'	            
+	  )
+
+PRINT char(13) + '* Inserting Values into orders table *';
+--insert values into orders
+BULK INSERT orders 
+FROM 'C:\TextFiles\orders.txt' 
+WITH (
+               CODEPAGE=1252,             
+		DATAFILETYPE = 'char',
+		FIELDTERMINATOR = '\t',
+		KEEPNULLS,
+		ROWTERMINATOR = '\n'	            
+	  )
+
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # SQL-Project
+
+https://docs.google.com/document/d/1frCoiHoRWjdeJxlRcNfOZE7QjhpcnhrD-Ztjd5fnA0Y/edit?usp=sharing
